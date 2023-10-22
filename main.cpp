53a44fdab0ac614490a68ba0f8c85a044739407d
Maria Denisova <mvdenisova.work@gmail.com>
2023-10-22 17:15:28 +0300

    completed task

--- main.cpp
+++ main.cpp
@@ -1,6 +1,58 @@
 #include <iostream>
+#include <fstream>
+using namespace std;
+
+struct TreeNode {
+    explicit TreeNode(int key) {
+        this->key = key;
+    }
+
+    int key;
+    TreeNode *left = nullptr;
+    TreeNode *right = nullptr;
+};
+
+class BinTree {
+public:
+    explicit BinTree() {
+        root = nullptr;
+    }
+
+    void Insert(int a) {
+        TreeNode **c = &root;
+        while (*c) {
+            TreeNode &n = **c;
+            if (a < n.key) {
+                c = &n.left;
+            } else if (a > n.key) {
+                c = &n.right;
+            } else {
+                return;
+            }
+        }
+        *c = new TreeNode(a);
+    }
+
+    TreeNode *root;
+};
+
+void Travel(TreeNode *v, std::ofstream &out) {
+    if (v) {
+        out << v->key << '\n';
+        Travel(v->left, out);
+        Travel(v->right, out);
+    }
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    ifstream in("input.txt");
+    int a;
+    BinTree tree;
+    while (in >> a) {
+        tree.Insert(a);
+    }
+    in.close();
+    ofstream out("output.txt");
+    Travel(tree.root, out);
+    out.close();
 }
